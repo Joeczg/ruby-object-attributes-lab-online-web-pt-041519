71030de7f446f3737101291f9f1e287499b59530
zhenggao cai <caizhengao1992@gmail.com>
2019-05-04 05:23:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,10 @@ class Dog
     @name = name
   end
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end
   def name 
-    @this_dogs_name
+    @name
   end
   def breed=(dogs_breed)
     @breed = dogs_breed
