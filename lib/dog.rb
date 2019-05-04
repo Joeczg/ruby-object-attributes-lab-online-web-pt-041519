5b76325e91fe2e3d6ab722ac49e0ef126f88ded0
zhenggao cai <caizhengao1992@gmail.com>
2019-05-04 05:23:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,16 @@ class Dog
   def initialize(name)
     @name = name
   end
-  def name
-    @name =name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
-  def name=(name)
-    @name = name
+  def name 
+    @this_dogs_name
+  end
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  def breed
+    @breed
   end
 end
